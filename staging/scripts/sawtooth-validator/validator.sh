622f56d53021eee727f8262129298ff340b95a69
Donald DeWulf <dewulf@younow.com>
2019-03-13 15:23:38 +0200

    Fix for validator endpoint

--- staging/scripts/sawtooth-validator/validator.sh
+++ staging/scripts/sawtooth-validator/validator.sh
@@ -121,7 +121,7 @@ handlers = [ "debug"]
 EOF
 
 sawtooth-validator  \
-    --endpoint tcp://$PROPSCHAIN_VALIDATOR_SERVICE_HOST:8800 \
+    --endpoint tcp://$SAWTOOTH_VALIDATOR_SERVICE_HOST:8800 \
     --bind component:tcp://eth0:4004 \
     --bind consensus:tcp://eth0:5050 \
     --bind network:tcp://eth0:8800 \
