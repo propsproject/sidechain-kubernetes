0d6a05f0b87fd055678f19783d87f4190a02ac99
Donald DeWulf <dewulf@younow.com>
2019-03-18 12:07:39 +0200

    downgrade back to 1.0.5

--- staging/scripts/sawtooth-validator/validator.sh
+++ staging/scripts/sawtooth-validator/validator.sh
@@ -1,47 +1,30 @@
+#!/bin/sh
+apt-get update
+apt-get install bash curl -y
+
 #!/bin/bash
 
-if [ ! -d "$SAWTOOTH_HOME/logs" ]; then
+if [ ! -e "$SAWTOOTH_HOME/logs" ]; then
     mkdir -p $SAWTOOTH_HOME/logs
 fi
 
-if [ ! -d "$SAWTOOTH_HOME/keys" ]; then
+if [ ! -e "$SAWTOOTH_HOME/keys" ]; then
     mkdir -p $SAWTOOTH_HOME/keys
 fi
 
-if [ ! -f "$SAWTOOTH_HOME/keys/validator.priv" ]; then
-    echo 'No validator private key found, generating one';
-    sawadm keygen;
-fi
-
-if [ ! -d "$SAWTOOTH_HOME/policy" ]; then
+if [ ! -e "$SAWTOOTH_HOME/policy" ]; then
     mkdir -p $SAWTOOTH_HOME/policy
 fi
 
-if [ ! -d "$SAWTOOTH_HOME/data" ]; then
+if [ ! -e "$SAWTOOTH_HOME/data" ]; then
     mkdir -p $SAWTOOTH_HOME/data
 fi
 
-if [ ! -d "$SAWTOOTH_HOME/etc" ]; then
+if [ ! -e "$SAWTOOTH_HOME/etc" ]; then
     mkdir -p $SAWTOOTH_HOME/etc
 fi
 
-if [ ! -e /opt/config-genesis.batch ]; then
-    echo "No config-genesis.batch file"
-    sawset genesis -k $SAWTOOTH_HOME/keys/validator.priv -o /opt/config-genesis.batch;
-fi
-
-if [ ! -e /opt/config.batch ]; then
-    sawset proposal create \
-    -k $SAWTOOTH_HOME/keys/validator.priv \
-    sawtooth.consensus.algorithm.name=Devmode \
-    sawtooth.consensus.algorithm.version=0.1 \
-    -o /opt/config.batch
-
-    sawadm genesis /opt/config-genesis.batch /opt/config.batch
-fi
-
 if [ ! -e "$SAWTOOTH_HOME/etc/validator.toml" ]; then
-    echo "Creating a sawtooth validator.toml configuration file"
     touch $SAWTOOTH_HOME/etc/validator.toml
     echo "opentsdb_url = \"${OPENTSDB_URL}\"" >> $SAWTOOTH_HOME/etc/validator.toml
     echo "opentsdb_db = \"${OPENTSDB_DB}\"" >> $SAWTOOTH_HOME/etc/validator.toml
@@ -50,25 +33,21 @@ if [ ! -e "$SAWTOOTH_HOME/etc/validator.toml" ]; then
     cat $SAWTOOTH_HOME/etc/validator.toml
 fi
 
+
 if [ ! -e "$SAWTOOTH_HOME/logs/validator-debug.log" ]; then
-    echo "Creating the validator-debug.log"
     touch $SAWTOOTH_HOME/logs/validator-debug.log
 fi
 
-if [ ! -e /root/.sawtooth/keys/my_key.priv ]; then
-    echo "No sawtooth key was found"
-    if [ -e /opt/my_key.priv ]; then
-        echo "Fetching the key from /opt"
-        mkdir -p /root/.sawtooth/keys
-        cp /opt/my_key.priv /root/.sawtooth/keys/my_key.priv
-        cp /opt/my_key.pub /root/.sawtooth/keys/my_key.pub
-    else
-        echo "Generating a new key"
-        sawtooth keygen my_key
-        cp /root/.sawtooth/keys/my_key.priv /opt/my_key.priv
-        cp /root/.sawtooth/keys/my_key.pub /opt/my_key.pub
-    fi
+if [ ! -e "$SAWTOOTH_HOME/keys/validator.priv" ]; then
+    sawadm keygen;
 fi
+
+if [ ! -e "$SAWTOOTH_HOME/data/block-chain-id" ]; then
+    sawadm genesis
+fi
+
+poet enclave basename --enclave-module simulator
+poet registration create --enclave-module simulator
 
 SH="$SAWTOOTH_HOME"
 env="$ENVIRONMENT"
@@ -109,9 +88,9 @@ handlers = [ "debug"]
 EOF
 
 sawtooth-validator  \
-    --endpoint tcp://$SAWTOOTH_VALIDATOR_SERVICE_HOST:8800 \
+    --endpoint tcp://$PROPSCHAIN_VALIDATOR_SERVICE_HOST:8800 \
     --bind component:tcp://eth0:4004 \
-    --bind consensus:tcp://eth0:5050 \
     --bind network:tcp://eth0:8800 \
+    --bind consensus:tcp://eth0:8800 \
     --opentsdb-url http://sawtooth-metrics:8086 \
     --opentsdb-db metrics
