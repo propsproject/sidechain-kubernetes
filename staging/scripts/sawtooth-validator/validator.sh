bdaa15546b364b250d1068ebe932fe5058b21b12
Donald DeWulf <dewulf@younow.com>
2019-03-20 10:23:57 +0200

    Fix consensus port

--- staging/scripts/sawtooth-validator/validator.sh
+++ staging/scripts/sawtooth-validator/validator.sh
@@ -91,7 +91,7 @@ sawtooth-validator  \
     --endpoint tcp://validator.staging.sidechain.propsproject.io:8800 \
     --bind component:tcp://eth0:4004 \
     --bind network:tcp://eth0:8800 \
-    --bind consensus:tcp://eth0:8800 \
+    --bind consensus:tcp://eth0:5050 \
     --peering static \
     --opentsdb-url http://sawtooth-metrics:8086 \
     --opentsdb-db metrics
