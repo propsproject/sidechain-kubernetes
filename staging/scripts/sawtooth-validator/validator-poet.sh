5295c784012fd50021803daedd01521d173a3aa6
Donald DeWulf <dewulf@younow.com>
2019-04-11 15:53:55 +0300

    Remove genesis

--- staging/scripts/sawtooth-validator/validator-poet.sh
+++ staging/scripts/sawtooth-validator/validator-poet.sh
@@ -69,7 +69,9 @@ if [ ! -e /opt/config.batch ]; then
     sawtooth.poet.ztest_minimum_win_count=999999999 \
     -o /opt/config.batch
 
-    #sawadm genesis /opt/config-genesis.batch /opt/config.batch /opt/poet.batch
+    sawadm genesis /opt/config-genesis.batch /opt/config.batch /opt/poet.batch
+else
+    rm $SAWTOOTH_HOME/data/genesis.batch
 fi
 
 if [ ! -e "$SAWTOOTH_HOME/etc/validator.toml" ]; then
