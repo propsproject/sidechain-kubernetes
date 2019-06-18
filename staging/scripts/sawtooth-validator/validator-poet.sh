7b81541eb5cc8549f318b3301720dd05667c0d98
Donald DeWulf <dewulf@younow.com>
2019-06-18 14:19:31 +0300

    Endpoint change

--- staging/scripts/sawtooth-validator/validator-poet.sh
+++ staging/scripts/sawtooth-validator/validator-poet.sh
@@ -151,7 +151,7 @@ handlers = [ "debug"]
 EOF
 
 sawtooth-validator  \
-    --endpoint tcp://validator.staging.sidechain.propsproject.io:8800 \
+    --endpoint $SAWTOOTH_VALIDATOR_PORT_8800_TCP \
     --bind component:tcp://eth0:4004 \
     --bind network:tcp://eth0:8800 \
     --bind consensus:tcp://eth0:5050 \
