796c91bdea7c53c96da8abaad52b8991e89fd5f4
Donald DeWulf <dewulf@younow.com>
2019-07-11 15:43:06 +0300

    Fix script

--- production-1.0/scripts/sawtooth-validator/validator.sh
+++ production-1.0/scripts/sawtooth-validator/validator.sh
@@ -25,6 +25,7 @@ if [ ! -e "$SAWTOOTH_HOME/etc" ]; then
 fi
 
 if [ ! -e "$SAWTOOTH_HOME/etc/validator.toml" ]; then
+    echo "[CREATING] Creating the validator.toml file"
     touch $SAWTOOTH_HOME/etc/validator.toml
     echo "opentsdb_url = \"${OPENTSDB_URL}\"" >> $SAWTOOTH_HOME/etc/validator.toml
     echo "opentsdb_db = \"${OPENTSDB_DB}\"" >> $SAWTOOTH_HOME/etc/validator.toml
@@ -35,34 +36,58 @@ fi
 
 
 if [ ! -e "$SAWTOOTH_HOME/logs/validator-debug.log" ]; then
+    echo "[CREATING] Creating the validator-debug.log file"
     touch $SAWTOOTH_HOME/logs/validator-debug.log
 fi
 
 if [ ! -e "$SAWTOOTH_HOME/keys/validator.priv" ]; then
+    echo "[CREATING] Creating validator priv/pub keys"
     sawadm keygen;
 fi
 
-if [ ! -e "$SAWTOOTH_HOME/data/block-chain-id" ]; then
-    sawadm genesis
+if [ ! -e "/opt/poet.batch" ]; then
+    echo "[CREATING] Poet genesis batch file"
 
     poet enclave basename --enclave-module simulator
-    poet registration create --enclave-module simulator
+    poet registration create --enclave-module simulator -k /opt/sawtooth/keys/validator.priv -o /opt/poet.batch
+fi
+
+if [ ! -e /opt/config-genesis.batch ]; then
+    echo "[CREATING] No config-genesis.batch file"
+    sawset genesis -k $SAWTOOTH_HOME/keys/validator.priv -o /opt/config-genesis.batch;
 fi
 
 if [ ! -e /root/.sawtooth/keys/root.priv ]; then
-    echo "No sawtooth key was found"
+    echo "No private key was found"
     if [ -e /opt/root.priv ]; then
         echo "Fetching the key from /opt"
         mkdir -p /root/.sawtooth/keys
         cp /opt/root.priv /root/.sawtooth/keys/root.priv
         cp /opt/root.pub /root/.sawtooth/keys/root.pub
     else
-        echo "Generating a new key"
+        echo "Generating a new key and adding the key to identity allowed keys"
         sawtooth keygen root
         cp /root/.sawtooth/keys/root.priv /opt/root.priv
         cp /root/.sawtooth/keys/root.pub /opt/root.pub
     fi
 fi
+
+if [ ! -e /opt/config.batch ]; then
+    echo "[CREATING] Creating a config.batch file"
+    sawset proposal create \
+    -k $SAWTOOTH_HOME/keys/validator.priv \
+    sawtooth.consensus.algorithm.name=PoET \
+    sawtooth.consensus.algorithm.version=0.1 \
+    sawtooth.settings.vote.authorized_keys=$(cat ~/.sawtooth/keys/root.pub),$(cat /opt/sawtooth/keys/validator.pub) \
+    sawtooth.identity.allowed_keys=$(cat /opt/root.pub),$(cat /opt/sawtooth/keys/validator.pub) \
+    sawtooth.validator.transaction_families='[{"family": "pending-earnings", "version": "1.0"},{"family":"sawtooth_settings","version":"1.0"},{"family":"sawtooth_identity","version":"1.0"},{"family":"sawtooth_validator_registry","version":"1.0"}]' \
+    -o /opt/config.batch
+
+    sawadm genesis /opt/config-genesis.batch /opt/config.batch
+    poet enclave basename --enclave-module simulator
+    poet registration create --enclave-module simulator
+fi
+
 
 SH="$SAWTOOTH_HOME"
 env="$ENVIRONMENT"
